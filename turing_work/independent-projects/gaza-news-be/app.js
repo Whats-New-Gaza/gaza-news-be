729abf8bb25b6f9fdb441622d7d36240660a3ec0
karimy <karim.alrashdan14@gmail.com>
2024-05-19 07:44:51 -0600

    Create get requests for newsOutlets, reporters, and users routes

--- turing_work/independent-projects/gaza-news-be/app.js
+++ turing_work/independent-projects/gaza-news-be/app.js
@@ -30,4 +30,42 @@ app.get("/articles", (req, res) => {
       res.status(500).json({ error: "Could not fetch the documents" })
     })
 })
-
+app.get("/newsOutlets", (req, res) => {
+  let newsOutlets = []
+  db.collection("newsOutlets")
+    .find()
+    .sort({ orgName: 1 })
+    .forEach((outlet) => newsOutlets.push(outlet))
+    .then(() => {
+      res.status(200).json(newsOutlets)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" })
+    })
+})
+app.get("/reporters", (req, res) => {
+  let reporters = []
+  db.collection("reporters")
+    .find()
+    .sort({ name: 1 })
+    .forEach((reporter) => reporters.push(reporter))
+    .then(() => {
+      res.status(200).json(reporters)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" })
+    })
+})
+app.get("/users", (req, res) => {
+  let users = []
+  db.collection("users")
+    .find()
+    .sort({ username: 1 })
+    .forEach((user) => users.push(user))
+    .then(() => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" })
+    })
+})
