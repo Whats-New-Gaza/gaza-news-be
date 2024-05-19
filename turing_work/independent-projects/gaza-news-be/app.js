430c6e4f945d877b2b76d1bfa5cfd676498f6b57
karimy <karim.alrashdan14@gmail.com>
2024-05-19 07:26:22 -0600

    Create get request for articles

--- turing_work/independent-projects/gaza-news-be/app.js
+++ turing_work/independent-projects/gaza-news-be/app.js
@@ -18,5 +18,16 @@ connectToDb((err) => {
 
 //routes
 app.get("/articles", (req, res) => {
-  res.json({ msg: "Welcome to the api" })
+  let articles = []
+  db.collection("articles")
+    .find()
+    .sort({ reporter: 1 })
+    .forEach((article) => articles.push(article))
+    .then(() => {
+      res.status(200).json(articles)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" })
+    })
 })
+
